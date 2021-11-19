90a8cdd4aca0993374a4c64c8048b23f5ce2d572
Serg-Sanhzura <alphapro100@gmail.com>
2021-11-19 02:07:14 +0200

    Зарегистрировал в settings.py honor and news

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'honor.apps.HonorConfig',
+    'news.apps.NewsConfig'
 ]
 
 MIDDLEWARE = [
