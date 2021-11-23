6c3b488dbed4eb3d1e6bf88e7ebea15adec4090c
Serg-Sanhzura <alphapro100@gmail.com>
2021-11-23 10:16:56 +0200

    переименовал news в home

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'honor.apps.HonorConfig',
-    'news.apps.NewsConfig'
+    'home.apps.HomeConfig'
 ]
 
 MIDDLEWARE = [
